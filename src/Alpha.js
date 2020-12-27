a020080f94a12fdca5d037c066bd2395de589004
Muhammad Wasim Arif <76242160+mwasim-arif@users.noreply.github.com>
2020-12-27 13:59:40 +0500

    Update few variations in css and js

---
+++ src/Alpha.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Alpha (props) {
+    return(
+        <h3>Value of Counter variable is: {props.counter}</h3>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
+import { useState } from 'react';
+import {Alpha} from './Alpha';
 import './App.css';
 
 function App() {
+
+  let [count, setCount] = useState(0);
+  let [isMorning, setMorning] = useState(true);
+
   return (
-    <div className='box'>
-      <h1>Hello World from wasim</h1>
+    <div className={`bold ${isMorning ? 'dayLight' : 'advan'} `}>
+      <h1>Have A Wonderful {isMorning ? 'Morning' : 'Night'}</h1>
+      <Alpha counter={count}/>
+      <br />
+      <button onClick={
+        ()=> setCount(++count) 
+      }>
+        Update Counter
+      </button>
+
+
+      <button onClick={()=> setMorning(!isMorning)}>
+        Update Day
+      </button>
+
+
+
     </div>
   );
 }
